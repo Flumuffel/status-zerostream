159e994a9715246f6355e7ef93a764de2688d835
Tadhg Lewis <tadhg.lewis@outlook.com>
2020-06-10 22:53:35 +1000

    Updates

---
+++ precache-manifest.5e5fd744e7b77a332dd288dd8121a08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e65645e9d5c7d66d36a088329aba02",
+    "url": "/tadhglewis/issue-status/index.html"
+  },
+  {
+    "revision": "8b6f47ab8bebfab24477",
+    "url": "/tadhglewis/issue-status/static/css/main.b427710c.chunk.css"
+  },
+  {
+    "revision": "fa6dd22adee41821e6d7",
+    "url": "/tadhglewis/issue-status/static/js/2.cbf271f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tadhglewis/issue-status/static/js/2.cbf271f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6f47ab8bebfab24477",
+    "url": "/tadhglewis/issue-status/static/js/main.23011dac.chunk.js"
+  },
+  {
+    "revision": "fc3fcf77e1202ea2cb7b",
+    "url": "/tadhglewis/issue-status/static/js/runtime-main.d75da592.js"
+  }
+]);
